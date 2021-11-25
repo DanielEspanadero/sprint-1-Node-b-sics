f07f0c6a7da623a43781bbc46f94504903ce40f8
Daniel Españadero <d.espanadero@gmail.com>
2021-11-25 10:42:34 +0100

    classWork

--- Sprint-1.4-Async-Await/1.4-async-and-await.js
+++ Sprint-1.4-Async-Await/1.4-async-and-await.js
@@ -22,5 +22,34 @@ let salaries = [{
     salary: 2000
 }];
 
-// Donats els objectes employees i salaries, crea una arrow function getEmployee que retorni una Promise efectuant la cerca en l'objecte pel seu id. Crea una altra arrow function getSalary que rebi com a paràmetre un objecte employee i retorni el seu salari.
+
+
+let getEmployee = id => {
+    return new Promise((resolve, reject) => {
+        if (id > 3 || id <= 0) {
+            reject(console.log(`El ID ${id} no es valido, tiene que ser entre 1 y 3`))
+
+        } else {
+            resolve(employees[id - 1]);
+        }
+    })
+}
+
+let id = 1
+let idEmpleado = id
+let employee = employees.find(employees => employees.id === idEmpleado);
+
+
+let getSalary = (employee) => {
+    return new Promise((resolve, reject) => {
+        if (id > 3 || id <= 0) {
+            reject(`El ID ${id} no es valido, tiene que ser entre 1 y 3`)
+
+        } else {
+            resolve(salaries[id - 1].salary);
+        }
+    })
+}
+
+// N1 E2: Crea una funció asíncrona que rebi un id d'empleat i imprimeixi per pantalla el nom de l'empleat i el seu salari, usant les funcions que has definit a l'exercici anterior.
 
